c1969b05e52410717672a879cea34b54249efc58
luca <aa0921284945@gmail.com>
2025-06-10 16:25:28 +0800

    Lab12 push

---
+++ Lab12/Lab12_Q1.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3; // Number of initialized elements in nums1
+    int n = 3; // Number of initialized elements in nums2
+
+    solution.merge(nums1, m, nums2, n);
+
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
